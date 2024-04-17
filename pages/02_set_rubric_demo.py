20439c242e4c344ecb8647f4d24cc016cee13d16
Daniel Lee <anjie.wav@gmail.com>
2024-04-16 22:46:57 -0700

    Updated UI for clarity

--- pages/02_set_rubric_demo.py
+++ pages/02_set_rubric_demo.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import main
 
-st.header("Welcome to :blue[_Set Rubric Demo_]", divider="rainbow")
+st.header(":rainbow[_set rubric demo_]", divider="rainbow")
 
 with st.form("debate_form", border=False):
 
-    context = st.text_input("Context", placeholder="Enter the body of your question here to improve grading accuracy. (Optional)", help="The user can optionally enter the body of the question for the program to use as a reference when grading the response. This is particularly useful for conditional rubric components, such as 'no irrelevant information'. The program will use the context as a reference to determine whether the response is relevant to the question.")
-    rubric_component = st.text_area("Rubric components", placeholder="Enter the grading rubric components here, starting each new component on a new line", help="The rubric component is what the response will be graded off of. If the rubric component is satisfied by the response, then 'true' is returned, else, 'false' is returned.")
-    student_response = st.text_input("Student response", placeholder="Enter the student response you want to grade here.", help="The student response is where the response to be graded is entered.")
+    context = st.text_area("Context", placeholder="Enter the body of your question here to improve grading accuracy. (Optional)", help="The user can optionally enter the body of the question for the program to use as a reference when grading the response. This is particularly useful for conditional rubric components, such as 'no irrelevant information'. The program will use the context as a reference to determine whether the response is relevant to the question.")
+    rubric_component = st.text_area(":red[*] Rubric components", placeholder="Enter the grading rubric components here, starting each new component on a new line", help="The rubric component is what the response will be graded off of. If the rubric component is satisfied by the response, then 'true' is returned, else, 'false' is returned.")
+    student_response = st.text_area(":red[*] Student response", placeholder="Enter the student response you want to grade here.", help="The student response is where the response to be graded is entered.")
     
     submitted = st.form_submit_button(label="Submit", help="Click to query the API to initiate a multi-agent debate session.")
         
