121c159b79c180c838389924f46ac03a88bc5fd0
Daniel Lee <anjie.wav@gmail.com>
2024-04-16 23:07:51 -0700

    Updated landing page

--- landing.py
+++ landing.py
@@ -3,8 +3,9 @@ import main
 
 st.header("Welcome to :blue[_mad-grader_]", divider="rainbow")
 st.markdown(''':blue[mad-grader] (working title) is an automatic grading program that utilizes multi-agent debate to ensure consistency and accuracy in delivering verdicts on whether a rubric component is satisfied.
-            \nThis is a demonstration of the multi-agent debate technology it utlizes. Specifically, the graders use OpenAI's GPT 3.5 and Anthropic's Claude 3 Haiku models, while the evaluator uses the same as the latter. We're currently working on implementing more models, such as Google's Gemini, as well as experimenting with locally-run LLMs for lower cost.
+            \nThis is a demonstration of the multi-agent debate technology it utlizes. Specifically, the graders use OpenAI's GPT 3.5 and Anthropic's Claude 3 Haiku models, while the evaluator uses the same as the latter. You can visit our GitHub repo [here](https://github.com/danleeaj/mad-grader/).
+            \nWe are currently working on creating a better user interface that allows for inputting multiple questions, with corresponding question weights.
             ''')
 
 st.divider()
-st.caption('Developed by An Jie Lee, 2024 // [Github](https://github.com/danleeaj)')
+st.caption('Developed by An Jie Lee, 2024 // [Github](https://github.com/danleeaj) | [Webpage](https://danleeaj.github.io/)')
