81253f9e4d02d155faa3054698cdfc2179a2afd0
Daniel Lee <anjie.wav@gmail.com>
2024-04-24 16:36:07 -0700

    Added API functionality

--- api.py
+++ api.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask_restful import Api, Resource
 
+from main import debate
+
 app = Flask(__name__)
 api = Api(app)
 
 class Debate(Resource):
-    def get(self, name):
-        return {"data": f"Get world, {name}"}
-    def post(self, name):
-        return {"data": f"{name}, post world"}
+    def get(self, rubric_component, student_response, context=None):
+        return debate(rubric_component, student_response, context, jsonify=True)
 
-api.add_resource(Debate, "/debate/<string:name>")
+api.add_resource(Debate, "/debate/<string:rubric_component>/<string:student_response>", "/debate/<string:rubric_component>/<string:student_response>/<string:context>")
 
 if __name__ == "__main__":
     app.run(debug=True)
